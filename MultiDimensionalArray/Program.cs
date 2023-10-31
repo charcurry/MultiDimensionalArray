25e6e5c8a5bfc45c3143d0490f04dc024977bb4e
Charlie <charlieryancurry@gmail.com>
2023-10-31 15:22:17 -0300

    Initial commit

---
+++ MultiDimensionalArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2D Arrays");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
